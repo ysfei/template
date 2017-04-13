400cf4aa1e43ec92ebd582be20af2f4501f35941
Levi Boxell <leviboxell@gmail.com>
2017-04-13 14:46:46 -0700

    test2

--- source/data/build.R
+++ source/data/build.R
@@ -3,7 +3,7 @@ PATHS <- yaml.load_file('constants.yaml')
 args  <- commandArgs(TRUE)
 
 main <- function(i){
-    x    <- seq(-10, 10, 0.2)
+    x    <- seq(-10, 10, 0.1)
     write.table(x, sprintf("%s/%s.txt", PATHS$build$data, i), sep = "|", 
                    row.names = FALSE, col.names = TRUE, quote = FALSE) 
 }
